9aebf95b376f4214f327dce3ff90ba38c3fb727f
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-03-19 13:24:58 -0700

    Can delete posts by id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -54,19 +54,19 @@ console.log(post);
 }
 });
 
+// DELETE request to /api/posts/:id
 router.delete('/:id', async (req, res) => {
 try {
     const count = await DB.remove(req.params.id);
     if (count > 0) {
     res.status(200).json({ message: 'The post has been deleted' });
     } else {
-    res.status(404).json({ message: 'The post could not be found' });
+    res.status(404).json({ message: 'The post with the specified ID does not exist.' });
     }
 } catch (error) {
-    // log error to database
     console.log(error);
     res.status(500).json({
-    message: 'Error removing the post',
+    message: 'The post could not be removed',
     });
 }
 });
